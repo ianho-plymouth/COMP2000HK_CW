5c6325643725a9887464c5b819f81a9691eb1dff
ianho-plymouth <chiu.m.ho@students.plymouth.ac.uk>
2025-12-01 03:42:40 +0800

    StaffMainActivity

---
+++ app/src/main/java/com/example/comp2000hk_cw/StaffMainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.comp2000hk_cw
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+
+class StaffMainActivity : ComponentActivity() {
+    private lateinit var btnEditMenu: Button
+    private lateinit var btnViewReservation: Button
+    private lateinit var btnLogout: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_staff_main)
+
+        btnEditMenu = findViewById(R.id.btnEditMenu)
+        btnViewReservation = findViewById(R.id.btnViewReservation)
+        btnLogout = findViewById(R.id.btnLogout)
+
+        btnEditMenu.setOnClickListener {
+            val intent = Intent(this, StaffMenuActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnViewReservation.setOnClickListener {
+            val intent = Intent(this, StaffReservationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnLogout.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
