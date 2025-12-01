a9a6620a3a2e37793dffae1cb4dcc261da580bbd
ianho-plymouth <chiu.m.ho@students.plymouth.ac.uk>
2025-12-01 12:38:52 +0800

    StaffSettingActivity

--- app/src/main/java/com/example/comp2000hk_cw/StaffMainActivity.kt
+++ app/src/main/java/com/example/comp2000hk_cw/StaffMainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.ComponentActivity
 class StaffMainActivity : ComponentActivity() {
     private lateinit var btnEditMenu: Button
     private lateinit var btnViewReservation: Button
+    private lateinit var btnSetting: Button
     private lateinit var btnLogout: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,7 @@ class StaffMainActivity : ComponentActivity() {
 
         btnEditMenu = findViewById(R.id.btnEditMenu)
         btnViewReservation = findViewById(R.id.btnViewReservation)
+        btnSetting = findViewById(R.id.btnSetting)
         btnLogout = findViewById(R.id.btnLogout)
 
         btnEditMenu.setOnClickListener {
@@ -26,6 +28,12 @@ class StaffMainActivity : ComponentActivity() {
 
         btnViewReservation.setOnClickListener {
             val intent = Intent(this, StaffReservationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnSetting.setOnClickListener {
+            val intent = Intent(this, StaffSettingActivity::class.java)
             startActivity(intent)
             finish()
         }
---
+++ app/src/main/java/com/example/comp2000hk_cw/StaffSettingActivity.kt
@@ -0,0 +1,37 @@
+package com.example.comp2000hk_cw
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+
+class StaffSettingActivity : ComponentActivity() {
+    private lateinit var btnBack: Button
+    private lateinit var btnReset: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_staff_setting)
+
+        btnBack = findViewById(R.id.btnBack)
+        btnReset = findViewById(R.id.btnReset)
+
+        btnBack.setOnClickListener {
+            val intent = Intent(this, GuestMainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btnReset.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Reset Password Success",
+                Toast.LENGTH_LONG
+            ).show()
+            val intent = Intent(this, GuestMainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
